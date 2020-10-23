bcdf48a59a533e490b5b1f2fe03e4abe95a3dfd3
azat1212 <grad317@mail.ru>
2020-10-23 19:21:16 +0300

    реализованны IEnumerable и IEnumerator

---
+++ 1/MyClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace _1
+{
+    public class MyClass<T> : IEnumerable, IEnumerator
+    {
+        public MyClass(T[] array)
+        {
+            this.array = array;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        private T[] array;
+        int position = -1;
+
+        public IEnumerator GetEnumerator()
+        {
+            return array.GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            if (position < array.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+        object IEnumerator.Current => throw new NotImplementedException();
+        public T Current
+        {
+            get
+            {
+                if (position == -1 || position >= array.Length)
+                    throw new InvalidOperationException();
+                return this[position];
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+
+
+    }
+}
---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> numbers = new MyClass<int>(new int[]{1,2,3,4,5});
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
